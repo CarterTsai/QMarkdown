f51169496b2439b2155c6bd20a2f78c046c338cb
Carter Tsai <hamming1@gmail.com>
2013-10-08 23:18:20 +0800

    Add convert and iInsert slot for markdown

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,33 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->editor->setText("cba");
+
+    connect(ui->editor, &QTextBrowser::textChanged,
+            this, &MainWindow::convert);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+int MainWindow::iInsert()
+{
+
+}
+
+// Slot of MainWindow
+// For handle convert markdown to html
+// Single receive from editor, when it text change
+int MainWindow::convert()
+{
+    Ui_MainWindow *ui = this->ui;
+    //
+
+    // reload markdown
+    //ui->markdown_viewer->setText("change word");
+    ui->markdown_viewer->reload();
+
+    return 0;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,10 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    int convert();
+    int iInsert();
+
 private:
     Ui::MainWindow *ui;
 };
